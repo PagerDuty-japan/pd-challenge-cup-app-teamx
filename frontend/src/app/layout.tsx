8df026b419c6f60ad103f50b8b85489734cf2912
Kazuto Kusama <jacopen@gmail.com>
2025-03-30 22:03:13 +0900

    update NR

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import Script from "next/script";
 import "./globals.css";
+import newrelic from "newrelic";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,20 +27,21 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const browserTimingHeader = newrelic.getBrowserTimingHeader({
+    hasToRemoveScriptWrapper: true,
+  });
   return (
     <html lang="en">
-      <Script id="newrelic" strategy="beforeInteractive">
-        {`
-window.NREUM||(NREUM={});NREUM.init={distributed_tracing:{enabled:true},privacy:{cookies_enabled:true},ajax:{deny_list:["bam.nr-data.net"]}};
-
-NREUM.loader_config={accountID:"4103586",trustKey:"3830171",agentID:"1589062825",licenseKey:"NRBR-a1037db8bd657923830",applicationID:"1589062825"};
-NREUM.info={beacon:"bam.nr-data.net",errorBeacon:"bam.nr-data.net",licenseKey:"NRBR-a1037db8bd657923830",applicationID:"1589062825",sa:1};
-        `}
-      </Script>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        {children}
+        <Script
+            id="nr-browser-agent"
+            strategy="beforeInteractive"
+            dangerouslySetInnerHTML={{ __html: browserTimingHeader }}
+        />
       </body>
     </html>
   );
