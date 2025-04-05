3f12d360dda731acbf3d1ef5c383e6d27158498b
Kazuto Kusama <jacopen@gmail.com>
2025-04-05 23:29:21 +0900

    Update setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
 sudo sudo yum -y install nmap-ncat bind-utils emacs
 aws eks --region ap-northeast-1 update-kubeconfig --name challenge-cup-cluster
 kubectl config set-context --current --namespace=teamx
+cd /tmp
+wget https://github.com/stern/stern/releases/download/v1.32.0/stern_1.32.0_linux_amd64.tar.gz
+tar xvf stern_1.32.0_linux_amd64.tar.gz
+sudo install stern /usr/local/bin/
